44a75f34528418192acb2b98c8ab804222a03d77
pavel <p.d.balykov@gmail.com>
2022-06-06 22:17:39 +0300

    exec programa

---
+++ README.md
@@ -0,0 +1,19 @@
+# Launching an application for unix systems
+
+### About the code
+
+The code was written in C++ using the qt framework.
+The UNIX library **#include <unistd.h>**
+for the ***execvp*** function that launches applications,
+the program also supports two languages Russian and English.
+
+### Launch
+
+after installation, go to the application\_launch folder and if you do it through idl, 
+then configure the compiler for yourself if the terminal is used, then
+
+``` 
+   qmake
+   make
+   ./execKod
+```
---
+++ mainwindow.cpp
@@ -0,0 +1,44 @@
+#include <QKeyEvent>
+#include <cstring>
+#include <unistd.h>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+static void freeArr(char** data, int size) {
+    for (auto i = 0; i < size; i++) {
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::on_runProg_clicked() {
+    QString text = this->ui->input->text();//.toStdString()
+    QStringList list = text.split( QRegExp("(\\ |\\t|\\n)") );
+    char** data = new char*[list.size() + 1];
+    for (auto i = 0; i < list.size(); i++) {
+        data[i] = new char[list[i].size() + 1];
+        std::strcpy(data[i], list[i].toLocal8Bit().constData());
+    }
+
+    data[list.size()] = NULL;
+    execvp(data[0], data);
+    freeArr(data, list.size());
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+    int key=event->key();
+    if ( key == Qt::Key_Return) {
+        this->on_runProg_clicked();
+    }
+}
+
